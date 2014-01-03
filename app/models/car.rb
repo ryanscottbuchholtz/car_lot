48cccce590cbe8de585a7686c10739be1a70b988
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2014-01-03 11:25:21 -0500

    Invalid year input tested and passing

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,5 @@
 class Car <ActiveRecord::Base
+  validates :year, 
+    presence: true,
+    numericality: { greater_than_or_equal_to: 1980, message: 'Model year not allowed. Year must be 1980 or later.' }
 end
--- spec/features/user_creates_new_car_record_spec.rb
+++ spec/features/user_creates_new_car_record_spec.rb
@@ -32,4 +32,22 @@ describe 'a user records a newly acquired car', %q{
 
     end
   end
+
+  context 'invalid information is provided (wrong car year)' do
+    it 'throws an error when year is earlier than 1980' do
+
+      car = FactoryGirl.create(:car)
+
+      visit new_car_path
+      fill_in 'Color', with: car.color
+      fill_in 'Mileage', with: car.mileage
+      fill_in 'Year', with: 1979
+      click_on 'Create new Car record'
+
+      expect(page).to have_content 'Model year not allowed. Year must be 1980 or later.'
+      expect(page).to_not have_content 'Congratulations.  Your car has been saved!'
+
+    end
+  end
+
 end
